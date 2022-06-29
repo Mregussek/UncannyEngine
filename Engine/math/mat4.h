c0be59437a8e6657051eeab2775840424865e483
Mateusz Rzeczyca <info@mateuszrzeczyca.pl>
2022-06-29 21:30:50 +0200

    added proper c++ doxygen docs to mat4, implemented tests for mat4

--- Engine/math/mat4.h
+++ Engine/math/mat4.h
@@ -18,10 +18,10 @@ struct mat4 {
 
   f32 elements[4 * 4]{ 0.f };
 
-  // @brief Default constructor
+  // @brief Default constructor, initializes all matrix elements to 0
   mat4() = default;
 
-  // @brief Explicit constructor for identity matrix creation
+  // @brief Explicit constructor for identity matrix creation with chosen diagonal
   explicit mat4(f32 diagonal);
 
   /*
@@ -38,10 +38,10 @@ struct mat4 {
 
   /*
    * @brief Row retrieval function.
-   * [ c0.x c0.y c0.z c0.w ]
-   * [ c1.x c1.y c1.z c1.w ]
-   * [ c2.x c2.y c2.z c2.w ]
-   * [ c3.x c3.y c3.z c3.w ]
+   * [ r0.x r0.y r0.z r0.w ]
+   * [ r1.x r1.y r1.z r1.w ]
+   * [ r2.x r2.y r2.z r2.w ]
+   * [ r3.x r3.y r3.z r3.w ]
    * @tparam T vec3 or vec4
    * @param index of row to be extracted
    * @return vec3 or vec4 typed row
@@ -67,7 +67,34 @@ struct mat4 {
    */
   template<typename T, typename U> U multiply(const T& other) const;
 
+  /*
+   * @brief Creates perspective (3D) matrix
+   * [ 1/(aspectRatio*tan(fov/2))  0              0                       0 ]
+   * [ 0                           1/tan(fov/2)   0                       0 ]
+   * [ 0                           0             -(far+near)/(far-near)  -1 ]
+   * [ 0                           0             -2*far*near/(far-near)   0 ]
+   * @param fov field of view
+   * @param aspectRatio aspect ratio of the image
+   * @param near how close near plane should be
+   * @param far how far away far plane should be
+   * @return constructed mat4 perspective matrix
+   */
   static mat4 perspective(f32 fov, f32 aspectRatio, f32 near, f32 far);
+
+  /*
+   * @brief Creates lookAt (view) matrix.
+   * fwd = normalize(center - eye)
+   * side = normalize(fwd x y)
+   * up = normalize(side x fwd)
+   * [ side.x       up.x      -fwd.x     0 ]
+   * [ side.y       up.y      -fwd.y     0 ]
+   * [ side.z       up.z      -fwd.z     0 ]
+   * [ -(side*eye) -(up*eye)  fwd*eye    1 ]
+   * @param eye viewer's (camera's) position
+   * @param center point at which camera stares
+   * @param y vector that tells, where is world heaven (where is y-axis)
+   * @return constructed lookAt matrix
+   */
   static mat4 lookAt(vec3 eye, vec3 center, vec3 y);
 
   /*
@@ -80,8 +107,32 @@ struct mat4 {
    * @return constructed translation matrix
    */
   static mat4 translation(vec3 t);
+
+  /*
+   * @brief Creates rotation matrix along given axis and angle.
+   * @param angle floating point value describing angle in radians
+   * @param axis vec3 describing axis along which rotation should be made
+   * @return constructed rotation matrix
+   */
   static mat4 rotation(f32 angle, vec3 axis);
+
+  /*
+   * @brief Creates scale matrix from given vec3.
+   * [ s.x 0   0   0 ]
+   * [ 0   s.y 0   0 ]
+   * [ 0   0   s.z 0 ]
+   * [ 0   0   0   1 ]
+   * @param t vec3 that describes scale along x, y and z-axis.
+   * @return constructed scale matrix
+   */
   static mat4 scale(vec3 params);
+
+  /*
+   * @brief Compares two matrices to each other and returns result.
+   * @param left first matrix
+   * @param right second matrix
+   * @return b32 value describing result (1 - the same, 0 - other)
+   */
   static b32 compare(const mat4& left, const mat4& right);
 
   f32 operator[](u32 index) const;
--- tests/math/mat4_tests.cpp
+++ tests/math/mat4_tests.cpp
@@ -302,6 +302,182 @@ TEST(Mat4TestSet, Translation) {
   */
 
   mat4 t = mat4::translation({ 2.f, 3.f, 4.f });
+  for(u32 i = 0; i < 12; i++) {
+    if (i == 0 || i == 5 || i == 10) {
+      EXPECT_EQ(t[i], 1.f);
+      EXPECT_EQ(t.elements[i], 1.f);
+      continue;
+    }
+    EXPECT_EQ(t[i], 0.f);
+    EXPECT_EQ(t.elements[i], 0.f);
+  }
+
+  EXPECT_EQ(t[12], 2.f);
+  EXPECT_EQ(t[13], 3.f);
+  EXPECT_EQ(t[14], 4.f);
+  EXPECT_EQ(t[15], 1.f);
+
+  EXPECT_EQ(t.elements[12], 2.f);
+  EXPECT_EQ(t.elements[13], 3.f);
+  EXPECT_EQ(t.elements[14], 4.f);
+  EXPECT_EQ(t.elements[15], 1.f);
+}
+
+
+TEST(Mat4TestSet, Scale) {
+  /*
+  * [ 2 0 0 0 ]
+  * [ 0 3 0 0 ]
+  * [ 0 0 4 0 ]
+  * [ 0 0 0 1 ]
+  */
+
+  mat4 t = mat4::scale({ 2.f, 3.f, 4.f });
+  for(u32 i = 0; i < 12; i++) {
+    if (i == 0) {
+      EXPECT_EQ(t[i], 2.f);
+      EXPECT_EQ(t.elements[i], 2.f);
+      continue;
+    }
+    if (i == 5) {
+      EXPECT_EQ(t[i], 3.f);
+      EXPECT_EQ(t.elements[i], 3.f);
+      continue;
+    }
+    if (i == 10) {
+      EXPECT_EQ(t[i], 4.f);
+      EXPECT_EQ(t.elements[i], 4.f);
+      continue;
+    }
+    if (i == 15) {
+      EXPECT_EQ(t[i], 1.f);
+      EXPECT_EQ(t.elements[i], 1.f);
+      continue;
+    }
+    EXPECT_EQ(t[i], 0.f);
+    EXPECT_EQ(t.elements[i], 0.f);
+  }
+}
+
+
+TEST(Mat4TestSet, Perspective) {
+  const f32 fov{ 2.f };
+  const f32 aspectRatio{ 16.f / 9.f };
+  const f32 near{ 0.001f };
+  const f32 far{ 100.f };
+
+  const f32 a{ 1.f / (aspectRatio * tan(fov / 2.f)) };
+  const f32 b{ 1.f / tan(fov / 2.f) };
+  const f32 c{ - (far + near) / (far - near) };
+  const f32 d{ -2 * far * near / (far - near) };
+
+  mat4 m{ mat4::perspective(fov, aspectRatio, near, far) };
+
+  EXPECT_EQ(m[0], a);
+  EXPECT_EQ(m[5], b);
+  EXPECT_EQ(m[10], c);
+  EXPECT_EQ(m[11], -1.f);
+  EXPECT_EQ(m[14], d);
+
+  for(u32 i = 0; i < 15; i++) {
+    if (i == 0 || i == 5 || i == 10 || i == 11 || i == 14) {
+      continue;
+    }
+    EXPECT_EQ(m[i], 0.f);
+    EXPECT_EQ(m.elements[i], 0.f);
+  }
+}
+
+
+static vec3 crossProduct(vec3 a, vec3 b) {
+  return {
+      a.y * b.z - b.y * a.z,
+      a.z * b.x - b.z * a.x,
+      a.x * b.y - b.x * a.y
+  };
+}
+
+
+static f32 dotProduct(vec3 a, vec3 b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+
+static vec3 normalizeVector(vec3 a) {
+  f32 length{ sqrt(dotProduct(a, a))  };
+  return a / length;
+}
+
+
+TEST(Mat4TestSet, LookAt) {
+  const vec3 eye{ 0.f, 0.f, 0.f };
+  const vec3 center{ 2.f, 1.f, -1.f };
+  const vec3 y{ 0.f, 1.f, 0.f };
+
+  mat4 m{ mat4::lookAt(eye, center, y) };
+
+  const vec3 fwd{ normalizeVector(center - eye) };
+  const vec3 side{ normalizeVector(crossProduct(fwd, y)) };
+  const vec3 up{ crossProduct(side, fwd) };
+
+  EXPECT_EQ(m[0], side.x);
+  EXPECT_EQ(m[1], up.x);
+  EXPECT_EQ(m[2], -fwd.x);
+  EXPECT_EQ(m[3], 0.f);
+
+  EXPECT_EQ(m[4], side.y);
+  EXPECT_EQ(m[5], up.y);
+  EXPECT_EQ(m[6], -fwd.y);
+  EXPECT_EQ(m[7], 0.f);
+
+  EXPECT_EQ(m[8], side.z);
+  EXPECT_EQ(m[9], up.z);
+  EXPECT_EQ(m[10], -fwd.z);
+  EXPECT_EQ(m[11], 0.f);
+
+  EXPECT_EQ(m[12], -dotProduct(side, eye));
+  EXPECT_EQ(m[13], -dotProduct(up, eye));
+  EXPECT_EQ(m[14], dotProduct(fwd, eye));
+  EXPECT_EQ(m[15], 1.f);
+}
+
+
+static f32 deg2rad(f32 deg) {
+  const f32 pi = 3.14159265359f;
+  return deg * pi / 180.f;
+}
+
+
+TEST(Mat4TestSet, Rotation) {
+  const f32 angle{ deg2rad(90.f) };
+  const vec3 axis{ 0.5f, 1.f, 0.f };
+
+  mat4 m{ mat4::rotation(angle, axis) };
+
+  const vec3 naxis{ normalizeVector(axis) };
+  const f32 x{ naxis.x };
+  const f32 y{ naxis.y };
+  const f32 z{ naxis.z };
+
+  EXPECT_EQ(m[0], cos(angle) + x * x * (1 - cos(angle)));
+  EXPECT_EQ(m[1], y * x * (1 - cos(angle)) + z * sin(angle));
+  EXPECT_EQ(m[2], z * x * (1 - cos(angle)) - y * sin(angle));
+  EXPECT_EQ(m[3], 0.f);
+
+  EXPECT_EQ(m[4], x * y * (1 - cos(angle)) - z * sin(angle));
+  EXPECT_EQ(m[5], cos(angle) + y * y * (1 - cos(angle)));
+  EXPECT_EQ(m[6], z * y * (1 - cos(angle)) + x * sin(angle));
+  EXPECT_EQ(m[7], 0.f);
+
+  EXPECT_EQ(m[8], x * z * (1 - cos(angle)) + y * sin(angle));
+  EXPECT_EQ(m[9], y * z * (1 - cos(angle)) - x * sin(angle));
+  EXPECT_EQ(m[10], cos(angle) + z * z * (1 - cos(angle)));
+  EXPECT_EQ(m[11], 0.f);
+
+  EXPECT_EQ(m[12], 0.f);
+  EXPECT_EQ(m[13], 0.f);
+  EXPECT_EQ(m[14], 0.f);
+  EXPECT_EQ(m[15], 1.f);
 }
 
 
