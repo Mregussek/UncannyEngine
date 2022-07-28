e02bc8855a41f61a9227372de40bad484a32d83a
Mateusz Rzeczyca <info@mateuszrzeczyca.pl>
2022-07-28 16:40:20 +0200

    commented window surface

--- Engine/renderer/vulkan/RenderContextVulkan.cpp
+++ Engine/renderer/vulkan/RenderContextVulkan.cpp
@@ -37,11 +37,11 @@ b32 FRenderContextVulkan::init(FRenderContextSpecification renderContextSpecs) {
 
   // We can create surface only with instance and check presentation support with
   // physical device that is why third step is window surface creation...
-  b32 properlyCreatedWindowSurface{ createWindowSurface() };
-  if (not properlyCreatedWindowSurface) {
-    UFATAL("Could not create window surface, rendering is not available!");
-    return UFALSE;
-  }
+  //b32 properlyCreatedWindowSurface{ createWindowSurface() };
+  //if (not properlyCreatedWindowSurface) {
+  //  UFATAL("Could not create window surface, rendering is not available!");
+  //  return UFALSE;
+  //}
 
   UINFO("Initialized Vulkan Render Context!");
   return UTRUE;
