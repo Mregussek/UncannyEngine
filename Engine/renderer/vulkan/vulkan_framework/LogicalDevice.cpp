594efa49edff8eb04da072663204a365907155d4
Mateusz Rzeczyca <mateusz.rzeczyca@nokia.com>
2022-10-10 23:31:50 +0200

    added first methods for logical device

--- Engine/renderer/vulkan/vulkan_framework/LogicalDevice.cpp
+++ Engine/renderer/vulkan/vulkan_framework/LogicalDevice.cpp
@@ -1,4 +1,24 @@
 
+#include "LogicalDevice.h"
+#include <utilities/Logger.h>
+
+
 namespace uncanny::vkf {
+
+
+b32 FLogicalDeviceVulkan::init(const FLogicalDeviceInitDependenciesVulkan& deps) {
+  UTRACE("Initializing logical device vulkan...");
+
+  UDEBUG("Initialized logical device vulkan!");
+  return UTRUE;
+}
+
+
+void FLogicalDeviceVulkan::terminate() {
+  UTRACE("Terminating logical device vulkan...");
+
+  UDEBUG("Terminated logical device vulkan!");
+}
+
 
 }
--- Engine/renderer/vulkan/vulkan_framework/LogicalDevice.h
+++ Engine/renderer/vulkan/vulkan_framework/LogicalDevice.h
@@ -3,7 +3,32 @@
 #define UNCANNYENGINE_LOGICALDEVICE_H
 
 
+#include <volk.h>
+#include <utilities/Variables.h>
+
+
 namespace uncanny::vkf {
+
+
+struct FLogicalDeviceInitDependenciesVulkan {
+  VkPhysicalDevice physicalDevice{ VK_NULL_HANDLE };
+  u32 queueFamilyIndexGraphics{ UUNUSED };
+  u32 queueFamilyIndexTransfer{ UUNUSED };
+};
+
+
+class FLogicalDeviceVulkan {
+public:
+
+  b32 init(const FLogicalDeviceInitDependenciesVulkan& deps);
+  void terminate();
+
+private:
+
+  VkDevice m_VkDevice{ VK_NULL_HANDLE };
+
+};
+
 
 }
 
