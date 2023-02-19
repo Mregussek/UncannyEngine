5a1914a54a89c08ea78295e90953b16e4ea5de0a
Mateusz Rzeczyca <mateuszrzeczyca17@gmail.com>
2023-02-19 20:11:06 +0100

    deleted stuff for new beginning

--- Engine/renderer/vulkan/vulkan_renderer/graphics_pipelines/shaders/buildShaders.bat
+++
@@ -1,7 +0,0 @@
-
-glslc.exe -fshader-stage=vertex colored_mesh.vert.glsl -o colored_mesh.vert.spv
-glslc.exe -fshader-stage=fragment colored_mesh.frag.glsl -o colored_mesh.frag.spv
-
-:: copy * D:\Projects\UncannyEngine\build\Sandbox\shaders
-
-pause
